a3fda781af1857b41101c1e09ae2d34b885eb782
Baptiste Georget <baptiste.georget@hotmail.com>
2023-04-08 12:32:57 +0200

    Correctif tower

--- Assets/Scripts/Player/MoneyUi.cs
+++ Assets/Scripts/Player/MoneyUi.cs
@@ -1,7 +1,6 @@
 using TMPro;
 using UnityEngine;
 
-
 public class MoneyUi : MonoBehaviour
 {
     public TextMeshProUGUI label;
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,17 +4,13 @@ public class Player : MonoBehaviour
 {
     public static int pv;
     public static int money;
-    public int startMoney = 400;
 
-    public static void Damage(int damage)
-    {
-        pv -= damage;
-    }
+    public int startPv = 10;
+    public int startMoney = 400;
 
     public void Start()
     {
         money = startMoney;
+        pv = startPv;
     }
-
-
 }
